689c58fc9b43c2a4a03dc31ca3a15c304f313619
bernadette breboneria <bernadette.breboneria@biarri.com>
2020-01-22 07:57:34 +0800

    added input type events

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
 
-
-const { buildSchema } = require('graphql');
-
-
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 const app = express();
 
-const events =[];
+const events = [];
 
 app.use(bodyParser.json());
 
-
-app.use('/graphql', graphqlHttp({
-    schema:buildSchema(`
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
      type Event {
         _id: ID!
         title: String!
@@ -44,26 +44,47 @@ app.use('/graphql', graphqlHttp({
         mutation: RootMutation
     }
     `),
-    rootValue:{
-        events: ()=>{
-            return events;
-        },
-        createEvent: (args)=>{
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description:args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            events.push(event);
-            return event;
-        }
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = new Event({
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+
+        events.push(event);
+        return event;
+        // return event.save().then(result =>{
+        //     return {...result._doc};
+        // }).catch(error =>{
+        //     throw error
+        // });
+      }
     },
-    graphiql:true
+    graphiql: true
+  })
+);
 
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-em7ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
-}))
-
+// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-em7ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+// .then(()=>{
+//     app.listen(3001);
+// })
+// .catch(err =>{
+//     console.log(err);
+// })
 
 app.listen(3001);
