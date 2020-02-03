a624d222a097a9f02d0f45735a6b3043d62c3633
bernadette breboneria <bernadette.breboneria@biarri.com>
2020-02-03 11:43:45 +0800

    connect to mongodbAtlas

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const mongoose = require("mongoose");
 const Event = require("./models/event");
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
 app.use(
@@ -50,41 +48,33 @@ app.use(
       },
       createEvent: args => {
         const event = new Event({
-          _id: Math.random().toString(),
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date)
         });
 
-        events.push(event);
-        return event;
-        // return event.save().then(result =>{
-        //     return {...result._doc};
-        // }).catch(error =>{
-        //     throw error
-        // });
+        return event
+          .save()
+          .then(result => {
+            return { ...result._doc };
+          })
+          .catch(error => {
+            throw error;
+          });
       }
     },
     graphiql: true
   })
 );
 
-const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-em7ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-em7ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
-// .then(()=>{
-//     app.listen(3001);
-// })
-// .catch(err =>{
-//     console.log(err);
-// })
-
-app.listen(3001);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-em7ev.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
